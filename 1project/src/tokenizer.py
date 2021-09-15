e6d6c45c721e9a4600b3c746061715ffad9d40b5
Chloe X <chloex2000@gmail.com>
2021-09-15 16:44:42 -0400

    have almost all of the stemmer done, but i am still wrinkling bugs with suffix 'ed'

--- 1project/src/tokenizer.py
+++ 1project/src/tokenizer.py
@@ -5,6 +5,7 @@ def tokenize_text(file):
         # print(lines)
     tokenized = []
     for line in lines:
+        # print(line)
         # step 1.1: all non-period punctuation is a word separator. remove punctuation
         # initializing punctuations string
         punc = '''!()-[]\{\};:'"\,<>/?@#$%^&*_~'''
@@ -18,11 +19,12 @@ def tokenize_text(file):
         for index in range(length):
             if line[index] == '.':
                 # check for single letter acronyms and replace . with ""
-                if line[index + 1] != " " and line[index + 2] == ".":
-                    temp += ""
-                    length -= 1
-                else: 
-                    temp += " "
+                if index < len(line) - 2:
+                    if line[index + 1] != " " and line[index + 2] == ".":
+                        temp += ""
+                        length -= 1
+                    else: 
+                        temp += " "
             else:
                 temp += line[index]
         line = temp
@@ -44,9 +46,10 @@ def remove_stopwords(tokens):
     for word in tokens:
         if word not in stopwords:
             temp = word
-            word = stem(word)
-            # print(f'word:{temp} stemmed: {word}')
-            vocab.append(word)
+            if len(word) != 1:
+                word = stem(word)
+                print(f'word:{temp} stemmed: {word}')
+                vocab.append(word)
     return vocab
 # step 3: Implement the first two steps of Porter stemming, as defined in the text. 
 """
@@ -66,54 +69,93 @@ step 1b
 """
 def stem(word):
     vowels = "aeiouy"
-    suffixSet1 = set(["sses", "s", "ied", "ies", "ies" "eed", "eedly", "ed", "edly", "ing"])
-    suffixSet2 = set(["at", "bl", "iz" ])
-    if len(word) == 1:
+    suffix = get_suffix(word)
+    if suffix == "sses":
+        print(f'word: {word} suffix: {suffix}')
+        word = word.replace(suffix, "ss")
         return word
-    for suffix in suffixSet1:
-        if word.endswith(suffix):
-            if suffix == "sses":
-                word = word.replace(suffix, "ss")
-            elif suffix == "ied" or suffix == "ies":
-                temp = word.replace(suffix, "")
-                if len(temp) > 1:
-                    word = word.replace(suffix, "i")
+    elif suffix == "ied" or suffix == "ies":
+        print(f'word: {word} suffix: {"ies"}')
+        temp = word.replace(suffix, "")
+        if len(temp) > 1:
+            word = word.replace(suffix, "i")
+        else:
+            word = word.replace(suffix, "ie")
+        return word
+    elif suffix == "s":
+        print(f'word: {word} suffix: {suffix}')
+        if word[-2] == 'e' or word[-2] not in vowels:
+            word = word[:-1]
+        return word
+    elif  suffix == "eed" or suffix == "eedly":
+        print(f'word: {word} suffix: {suffix}')
+        # TODO: how to find FIRST non-vowel following a vowel
+        """
+        Iterate through letter for first non vowel after a vowel
+        then check for next vowel if = to suffix
+            if yes, word = word.replace(suffix, "ee")
+            else: break early
+        """
+        print(f'{word} {suffix}')
+        first_vowel = False
+        non_vowel = False
+        for index in range(len(word)):
+            if word[index] in vowels and not first_vowel:
+                first_vowel = True
+            elif word[index] not in vowels and first_vowel:
+                non_vowel = True
+            elif word[index] in vowels and first_vowel and non_vowel:
+                # conditional above means: in part of word after first non-vowel following a vowel
+                if word[index:] == suffix:
+                    word = word.replace(suffix, "ee")
                 else:
-                    word = word.replace(suffix, "ie")
-            elif suffix == "s":
-                if word[-2] == 'e' or word[-2] not in vowels:
-                    word = word.replace(word[-1], "")
-            elif suffix == "eed" or suffix == "eedly":
-                # TODO: how to find FIRST non-vowel following a vowel
-                """
-                Iterate through letter for first non vowel after a vowel
-                then check for next vowel if = to suffix
-                    if yes, word = word.replace(suffix, "ee")
-                    else: break early
-                """
-            elif suffix == "ed" or suffix == "edly" or suffix == "ing" or suffix == "ingly":
-                # TODO:
-                temp = word.replace(suffix, "")
-                for letter in reversed(temp):
-                    # print(f'letter: {letter} temp: {temp}')
-                    if letter in vowels:
-                        word = word.replace(suffix, "")
-                        # print(f'word: {word}')
-                        if word.endswith("at") or word.endswith("bl") or word.endswith("iz"):
-                            # unit tested for pirated, pirating
-                            word += "e"
-                        elif word[-1] == word[-2] and (word[-2:] != "ll" and word[-2:] != "ss" and word[-2:] != "zz"):
-                            # print(f'word: {word}, word[-1]:{word[-1]}')
-                            # unit tested for
-                            word = word[:-1]
-                            # print(word)
-                        elif len(word) <= 3:
-                            # unit tested for hoping
-                            word += "e"
-                        # print("Break")
-                        break
+                    break
+        return word
+    elif suffix == "ed" or suffix == "edly" or suffix == "ing" or suffix == "ingly":
+        print(f'word: {word} suffix: {suffix}')
+        temp = word.replace(suffix, "")
+        print(f'temp {temp}')
+        for letter in reversed(temp):
+            if letter in vowels:
+                word = word.replace(suffix, "")
+                if word.endswith("at") or word.endswith("bl") or word.endswith("iz"):
+                    word += "e"
+                elif word[-1] == word[-2] and (word[-2:] != "ll" and word[-2:] != "ss" and word[-2:] != "zz"):
+                    word = word[:-1]
+                elif len(word) <= 3 and word[-1] != "y":
+                    word += "e"
+                return word 
     return word
 
+def get_suffix(word):
+    suffixSet1 = set(["sses", "ss", "us", "s", "ied", "ies", "eed", "eedly", "ed", "edly", "ing"])
+    suffix = ""
+    print("line 132")
+    if word.endswith("sses"):
+        suffix = "sses"
+    elif word.endswith("ss"):
+        return suffix
+    elif word.endswith("us"):
+        return suffix
+    elif word.endswith("s"):
+        suffix = "s"
+    elif word.endswith("ied"):
+        suffix = "ied"
+    elif word.endswith("ies"):
+        suffix = "ies"
+    elif word.endswith("eed"):
+        suffix = "eed"
+    elif word.endswith("ed"):
+        print("line 148")
+        suffix == "ed"
+    elif word.endswith("eedly"):
+        suffix = "eedly"
+    elif word.endswith("edly"):
+        suffix = "edly"
+    elif word.endswith("ing"):
+        suffix = "ing"
+    print(f'suffix: {suffix}')
+    return suffix
 
 def tokenization(file):
     tokenized = tokenize_text(file)
@@ -124,10 +166,6 @@ def tokenization(file):
 
 # testing
 # tokenization("tokenization-input-part-A.txt")
-tokenization("test.txt")
-# word = "word"
-# print(word[-2:])
-# vowels = "aeiou"
-# if word[-2] not in vowels:
-#     word = word.replace(word[-1], "")
-# print(word)
+tokenization("test2.txt")
+
+# tokenization("tokenization-input-part-B.txt")
